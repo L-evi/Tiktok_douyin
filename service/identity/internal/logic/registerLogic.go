1458a935ab8ea1ccbe9cfea9db4bd172c85643a1
soxft <email@xcsoft.top>
2023-01-16 17:03:27 +0800

    feta: framework

---
+++ service/identity/internal/logic/registerLogic.go
@@ -0,0 +1,35 @@
+package logic
+
+import (
+	"context"
+
+	"train-tiktok/service/identity/internal/svc"
+	"train-tiktok/service/identity/types/identity"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type RegisterLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewRegisterLogic(ctx context.Context, svcCtx *svc.ServiceContext) *RegisterLogic {
+	return &RegisterLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *RegisterLogic) Register(in *identity.RegisterReq) (*identity.RegisterResp, error) {
+	return &identity.RegisterResp{
+		Response: &identity.Resp{
+			StatusCode: 200,
+			StatusMsg:  "sss",
+		},
+		UserId: 0,
+		Token:  "",
+	}, nil
+}
---
+++ service/identity/internal/svc/serviceContext.go
@@ -0,0 +1,13 @@
+package svc
+
+import "train-tiktok/service/identity/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
