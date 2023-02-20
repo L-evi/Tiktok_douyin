1ee02ffe5217ed02e8f81313fd42fbdc983c2bdb
soxft <email@xcsoft.top>
2023-02-21 01:05:22 +0800

    fix

--- service/chat/models/chat.go
+++ service/chat/models/chat.go
@@ -5,5 +5,5 @@ type Chat struct {
 	FromUserId int64  `gorm:"index;"`
 	ToUserId   int64  `gorm:"index;"`
 	Content    string `gorm:"type:text"`
-	CreateAt   int64  `gorm:"autoUpdateTime:milli;index"`
+	CreateAt   int64  `gorm:"index;autoUpdateTime:milli"`
 }
